d96668033ba25fcfb0bad751200b0c6fe9707f1f
Papacia <papacia.rich11@gmail.com>
2025-08-02 17:58:51 +0800

    update

--- func/askGemini.js
+++ func/askGemini.js
@@ -1,13 +1,9 @@
 export async function askGemini(prompt) {
   const API_KEYS = [
-    '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',
-    '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',
-    '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',
-    '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',
-    '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'
+   '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'
   ];
 
-  const model = 'google/gemma-3-27b-it:free';
+  const model = 'qwen/qwen3-30b-a3b-instruct-2507';
 
   for (const key of API_KEYS) {
     try {
